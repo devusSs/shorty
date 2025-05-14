5f0289ac6dab666983b21167f60ea5646bb4046b
devusSs <flofest98@gmail.com>
2025-05-14 22:39:22 +0200

    added main server package with env loading and db connection

---
+++ cmd/server/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/devusSs/shorty/pkg/env"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	if isDev() {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+		env.SetEnvFile(".env")
+		env.SetPrefix("DEV_SHORTY_")
+		logWarn("debug mode enabled, might leak sensitive data")
+	}
+
+	env, err := env.Load()
+	if err != nil {
+		logError("failed to load env", slog.Any("err", err))
+		return 1
+	}
+
+	logDebug("loaded env", slog.Any("env", env))
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	dbConn, err := pgxpool.New(ctx, env.PostgresDSN)
+	if err != nil {
+		logError("failed to establish db connection", slog.Any("err", err))
+		return 1
+	}
+	defer dbConn.Close()
+
+	logDebug("connected to database", slog.String("dsn", env.PostgresDSN))
+
+	return 0
+}
+
+func isDev() bool {
+	dev := os.Getenv("SHORTY_DEVELOPMENT")
+	b, err := strconv.ParseBool(dev)
+	return err == nil && b
+}
+
+func logWarn(msg string, args ...any) {
+	slog.With("prefix", "main").Warn(msg, args...)
+}
+
+func logError(msg string, args ...any) {
+	slog.With("prefix", "main").Error(msg, args...)
+}
+
+func logDebug(msg string, args ...any) {
+	slog.With("prefix", "main").Debug(msg, args...)
+}
