5a28650759ad2f5e912ec3b00b295174caa4d77f
devusSs <flofest98@gmail.com>
2025-05-15 15:02:07 +0200

    added hashing package to hash password and compare password hashes

---
+++ internal/hashing/hashing.go
@@ -0,0 +1,18 @@
+package hashing
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+func ComparePasswordHash(password string, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
